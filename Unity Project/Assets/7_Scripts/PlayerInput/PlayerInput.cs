6330a88e8252536e51a834d5400840549b5f0fd7
EdwardShakhov <edwardshakhov@gmail.com>
2021-09-19 18:21:16 +0300

    Input + Camera

--- Unity Project/Assets/7_Scripts/PlayerInput/PlayerInput.cs
+++ Unity Project/Assets/7_Scripts/PlayerInput/PlayerInput.cs
@@ -15,11 +15,11 @@ public class PlayerInput : MonoBehaviour
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical);
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
         if (direction.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg + camera.eulerAngles.y;
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
