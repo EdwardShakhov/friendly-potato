64123185e0f8bef857f6312734201965526a1599
EdwardShakhov <edwardshakhov@gmail.com>
2021-09-24 02:27:55 +0300

    idk code

--- Unity Project/Assets/Scripts/PlayerInput.cs
+++ Unity Project/Assets/Scripts/PlayerInput.cs
@@ -5,18 +5,27 @@ using UnityEngine;
 public class PlayerInput : MonoBehaviour
 {
     public CharacterController controller;
-    public Transform camera;
-    public float speed = 1f;
-    public float turnSmoothTime = 0.1f;
-    float turnSmoothVelocity;
 
-    // Update is called once per frame
-    void Update()
+    public void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+    }
+}
 
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 1f;
+    public float turnSmoothTime = 0.1f;
+    public Animator animator;
+
+    new public Transform camera;
+    public float turnSmoothVelocity;
+
+    public void Update()
+    {
+        var direction = PlayerInput.GetDirection();
         if (direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
