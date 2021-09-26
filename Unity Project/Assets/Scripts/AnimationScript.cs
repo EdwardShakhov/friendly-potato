fb806797dd11e1835d9d189ff983ab962a40cb4b
EdwardShakhov <edwardshakhov@gmail.com>
2021-09-26 22:03:36 +0300

    Code edited

--- Unity Project/Assets/Scripts/AnimationScript.cs
+++ Unity Project/Assets/Scripts/AnimationScript.cs
@@ -5,27 +5,15 @@ using UnityEngine;
 public class AnimationScript : MonoBehaviour
 {
     Animator animator;
-    int isWalkingHash;
 
     void Start()
     {
         animator = GetComponent<Animator>();
-        isWalkingHash = Animator.StringToHash("isWalkingForward");
     }
 
     void Update()
     {
-        bool isWalkingForward = animator.GetBool(isWalkingHash);
-        bool press_WASD = Input.GetKey("w") || Input.GetKey("a") || Input.GetKey("d") || Input.GetKey("s");
-
-        if (!isWalkingForward && press_WASD)
-        {
-            animator.SetBool(isWalkingHash, true);
-        }
-
-        if (isWalkingForward && !press_WASD)
-        {
-            animator.SetBool(isWalkingHash, false);
-        }
+        float move = Input.GetAxis("Vertical");
+        animator.SetFloat("speed", move);
     }
 }
--- Unity Project/Assets/Scripts/PlayerController.cs
+++ Unity Project/Assets/Scripts/PlayerController.cs
@@ -1,31 +1,33 @@
 using UnityEngine;
 using UnityEngine.Serialization;
 
-namespace Game
+namespace Game  //what for?
 {
     public class PlayerController : MonoBehaviour
     {
-        public float speed = 1f;
-        public float turnSmoothTime = 0.1f;
-        public Animator animator;
-
-        public Transform gameCamera;
-        public float turnSmoothVelocity;
 
         public PlayerInput input;
+        public CharacterController controller;
+        public Transform gameCamera;
+        public Animator animator;
+
+        public float speed;
+        public float turnSmoothTime = 0.1f;
+        
+        public float turnSmoothVelocity;
+
         public void Update()
         {
             var direction = input.GetDirection();
             
-            
-            if (direction.magnitude >= 0.1f)
+            if (direction.magnitude >= 0.5f)
             {
                 float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + gameCamera.eulerAngles.y;
                 float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
                 transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
                 Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            //    controller.Move(moveDir.normalized * speed * Time.deltaTime);
+                controller.Move(moveDir.normalized * speed * Time.deltaTime);
             }
         }
     }
