732e29a6c5bce5ab6555bb8c6c89f2b3eeb91d23
EdwardShakhov <edwardshakhov@gmail.com>
2021-10-08 04:32:10 +0300

    Game Manager added

--- Unity Project/Assets/Scripts/GameManager.cs
+++ Unity Project/Assets/Scripts/GameManager.cs
@@ -1,16 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    protected void Start()
+    private static GameManager _instance;
+    public static GameManager Instance
     {
-        
+        get
+        {
+            if (_instance == null)
+            {
+                Debug.LogError("GameManager instance not specified");
+            }
+            return _instance;
+        }
     }
-    
-    protected void Update()
+
+    protected void Awake()
     {
-        
+        _instance = this;
+    }
+
+    protected void OnDestroy()
+    {
+        if (_instance == this) ;
+        {
+            _instance = null;
+        }
     }
 }
