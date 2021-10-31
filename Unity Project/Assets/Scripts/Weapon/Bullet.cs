7cfaabc7efa4ad46dcc38f6e002b6f30b7b32743
EdwardShakhov <edwardshakhov@gmail.com>
2021-10-31 03:39:04 +0300

    Update Bullet.cs
    Destroys bullet by distance from player

--- Unity Project/Assets/Scripts/Weapon/Bullet.cs
+++ Unity Project/Assets/Scripts/Weapon/Bullet.cs
@@ -2,11 +2,22 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    private const float _bulletDistance = 30;
     protected void Start()
     {
-        Destroy(gameObject, 3.0f);
+        //Destroys bullet after 3 seconds
+        //Destroy(gameObject, 3.0f);
     }
 
+    protected void Update()
+    {
+        //Destroys bullet when it's far from player
+        if (Vector3.Distance(GameManager.Instance.Player.transform.position, gameObject.transform.position) > _bulletDistance)
+        {
+            Destroy(gameObject);
+        }
+}
+    
     protected void OnCollisionEnter()
     {
         Destroy(gameObject);
